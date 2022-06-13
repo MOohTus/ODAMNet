350c0ad2af40a493611c584143f8f3f259032d1a
MOohTus <caco630@hotmail.fr>
2022-06-13 16:18:54 +0200

    DOMINO request is working!

--- DOMINO_request.py
+++ DOMINO_request.py
@@ -13,25 +13,24 @@ import os
 
 # Change work directory
 os.chdir('/home/morgane/Documents/05_EJPR_RD/WF_Environment/EnvironmentProject/test/InputData/')
-os.chdir('D:\\Morgane\\Work\\MMG\\05_EJP_RD\\WF_Environment\\EnvironmentProject\\test\\InputData\\')
+## os.chdir('D:\\Morgane\\Work\\MMG\\05_EJP_RD\\WF_Environment\\EnvironmentProject\\test\\InputData\\')
 
 # Read files and create dict of them
 # geneSet = '/home/morgane/Documents/05_EJPR_RD/WF_Environment/EnvironmentProject/test/InputData/InputFile_DOMINO_tnfa_active_genes_file.txt'
 # network = '/home/morgane/Documents/05_EJPR_RD/WF_Environment/EnvironmentProject/test/InputData/InputFile_DOMINO_string.sif'
 
-geneSet = 'InputFile_DOMINO_tnfa_active_genes_file.txt'
+## geneSet = 'InputFile_DOMINO_tnfa_active_genes_file.txt'
+geneSet = 'InputFile_DOMINO_D014801.txt'
 network = 'InputFile_DOMINO_string.sif'
 
 data_dict = {
     'Network file name': network,
     'Active gene file name': geneSet
 }
-
 files_dict = {
     'Network file contents': open(network, 'rb'),
     'Active gene file contents': open(geneSet, 'rb')
 }
-
 response = requests.post(url='http://domino.cs.tau.ac.il/upload', data=data_dict, files=files_dict)
 response
 response.text
