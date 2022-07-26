864e04b0bf7e7c2289aa2b5e3c24938e638bde9a
Morgane Terezol <10067410+MOohTus@users.noreply.github.com>
2022-07-26 15:00:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='EnvironmentProject', version = '1.0', packages=find_packages())
+# Read the contents of the README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.rst').read_text()
+
+setup(name='EnvironmentProject',
+      version = '1.0', 
+      packages=find_packages(),
+      long_description=long_description)
